31207a481ed91a3166d6ffafb9f4a32956c4e5ca
Paul Leonard <paul_leonard88@hotmail.com>
2025-02-16 18:11:42 +0000

    remove dups sorted list leetcode

---
+++ LeetCode.Test/Easy/RemoveDuplicatesSortedArrayTest.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Easy;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Easy
+{
+    class RemoveDuplicatesSortedArrayTest
+    {
+        [Test]
+        public void SampleTest1()
+        {
+            int[] nums = [1, 1, 2];
+            var result = RemoveDuplicatesSortedArray.RemoveDuplicates(nums);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void SampleTest2()
+        {
+            int[] nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+            var result = RemoveDuplicatesSortedArray.RemoveDuplicates(nums);
+
+            Assert.That(result, Is.EqualTo(5));
+        }
+    }
+}
---
+++ LeetCode/Easy/RemoveDuplicatesSortedArray.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Easy
+{
+    //https://leetcode.com/problems/remove-duplicates-from-sorted-array/?envType=study-plan-v2&envId=top-interview-150
+
+    //Hint, They are checking the value of the array afterwards 
+    //so change in place
+    public static class RemoveDuplicatesSortedArray
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+
+            var index = 1;
+            for (var i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i - 1])
+                {
+                    nums[index++] = nums[i];
+                }
+            }
+            return index;
+        }
+    }
+}
